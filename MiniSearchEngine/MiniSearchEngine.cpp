4fe2d5d4e14bf05eaa2701da034d8e27e6028426
viethung9at1 <viethung.9at1@gmail.com>
2021-07-15 09:59:46 +0700

    Update for CMake use

---
+++ MiniSearchEngine/MiniSearchEngine.cpp
@@ -0,0 +1,12 @@
+﻿// MiniSearchEngine.cpp : Defines the entry point for the application.
+//
+
+#include "MiniSearchEngine.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
