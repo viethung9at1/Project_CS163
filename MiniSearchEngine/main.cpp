57806ed11305e1001f635c59eaf51584a3f1140b
viethoang1211 <viethoang12112002@gmail.com>
2021-07-20 20:05:05 +0700

    Update main.cpp
    Nothing, just test

--- MiniSearchEngine/main.cpp
+++ MiniSearchEngine/main.cpp
@@ -5,9 +5,22 @@ using namespace std;
 
 int main()
 {
-	SearchEngine Google;
-	Google.loadData();
-	Google.run();
+	// SearchEngine Google;
+	// Google.loadData();
+	// Google.run();
 
-	//system("pause");
+	TrieNode* root = new TrieNode();
+	root->insert("abcd", 2, 1);
+	root->insert("abcd", 8, 0);
+	root->insert("abc", 10, 0);
+
+	TrieNode* find = root->search("abc", 0);
+	if (find) {
+		for (int i : find->occurs)
+			cout << i << ' ';
+		cout << '\n';
+	}
+	else cout << "not found\n";
+
+	system("pause");
 }
