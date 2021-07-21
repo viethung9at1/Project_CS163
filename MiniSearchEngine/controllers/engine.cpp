5b0c62f72f6b4d222bf0850bb5d080de3b769ae7
khoa <dknguyen176@gmail.com>
2021-07-21 11:01:09 +0700

    Update engine.cpp

--- MiniSearchEngine/controllers/engine.cpp
+++ MiniSearchEngine/controllers/engine.cpp
@@ -1,19 +1,31 @@
 #include "../models/SearchEngine.h"
 
-bool result::operator< (result b) {
-	return occurs.size() < b.occurs.size();
+result::result(string filename, vector<int> occurs) : filename(filename), occurs(occurs) {
 }
-vector<result> SearchEngine::searchQuery(string text) {
-	return vector<result>();
+bool result::operator< (result b) {
+	return occurs.size() > b.occurs.size() || (occurs.size() == b.occurs.size() && filename < b.filename);
 }
 bool SearchEngine::isStopWord(string key) {
 	return stopWords->search(key, 0);
 }
 vector<int> SearchEngine::combineOccurs(vector<int> occur1, vector<int> occur2) {
-	if (!occur2.size()) return occur1;
-	for (int i : occur1) if (occur2[0] == i) return occur1;
-	vector<int> occurs(occur1.size() + occur2.size());
-	occurs.insert(occurs.end(), occur1.begin(), occur1.end());
-	occurs.insert(occurs.end(), occur2.begin(), occur2.end());
+	vector<int> occurs; int i = 0, j = 0;
+	while (i < occur1.size() || j < occur2.size()) {
+		while (j == occur2.size() || (i < occur1.size() && j < occur2.size() && occur1[i] < occur2[j])) occurs.push_back(occur1[i++]);
+		while (i == occur1.size() || (i < occur1.size() && j < occur2.size() && occur1[i] > occur2[j])) occurs.push_back(occur2[j++]);
+		while (i < occur1.size() && j < occur2.size() && occur1[i] == occur2[j]) occurs.push_back(occur1[i]), i++, j++;
+	}
 	return occurs;
+}
+vector<result> SearchEngine::searchQuery(string text) {
+	vector<result> results;
+	for (string filename : filenames) {
+		vector<int> search = searchQuery(filename, text);
+		if (search.size()) results.push_back(result(filename, search));
+	}
+	sort(results.begin(), results.end());
+	return results;
+}
+vector<int> SearchEngine::searchQuery(string filename, string text) {
+	return vector<int>();
 }
