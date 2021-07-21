f2d7956ae373b72808b133a833ac6d384e2fd119
khoa <dknguyen176@gmail.com>
2021-07-21 15:14:40 +0700

    Update search query

--- MiniSearchEngine/controllers/engine.cpp
+++ MiniSearchEngine/controllers/engine.cpp
@@ -6,24 +6,20 @@ bool result::operator< (result b) {
 	return occurs.size() > b.occurs.size() || (occurs.size() == b.occurs.size() && filename < b.filename);
 }
 bool SearchEngine::isStopWord(string key) {
-	return stopWords->search(key, 0);
-}
-vector<int> SearchEngine::combineOccurs(vector<int> occur1, vector<int> occur2) {
-	vector<int> occurs; int i = 0, j = 0;
-	while (i < occur1.size() || j < occur2.size()) {
-		while (j == occur2.size() || (i < occur1.size() && j < occur2.size() && occur1[i] < occur2[j])) occurs.push_back(occur1[i++]);
-		while (i == occur1.size() || (i < occur1.size() && j < occur2.size() && occur1[i] > occur2[j])) occurs.push_back(occur2[j++]);
-		while (i < occur1.size() && j < occur2.size() && occur1[i] == occur2[j]) occurs.push_back(occur1[i]), i++, j++;
-	}
-	return occurs;
+	return (stopWords->search(key, 0)).size();
 }
 vector<result> SearchEngine::searchQuery(string text) {
 	vector<result> results;
 	for (string filename : filenames) {
-		vector<int> search = searchQuery(filename, text);
-		if (search.size()) results.push_back(result(filename, search));
+		result search = result(filename, searchQuery(filename, text));
+
+		int i = 0;
+		while (i < results.size() && results[i] < search) i = i + 1;
+
+		results.push_back(search);
+		for (int j = results.size() - 1; j > i; --j) swap(results[j], results[j - 1]);
+		if (results.size() > 5) results.pop_back();
 	}
-	sort(results.begin(), results.end());
 	return results;
 }
 vector<int> SearchEngine::searchQuery(string filename, string text) {
