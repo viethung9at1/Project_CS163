ec48a87cbe809ff03d4cf0266a583dd352b325a8
khoa <dknguyen176@gmail.com>
2021-07-21 11:42:25 +0700

    Default main.cpp

--- MiniSearchEngine/main.cpp
+++ MiniSearchEngine/main.cpp
@@ -6,22 +6,8 @@ using namespace std;
 int main()
 {
 	SearchEngine Google;
-	// Google.loadData();
-	// Google.run();
-	vector<result> results = Google.searchQuery("steve jobs");
+	Google.loadData();
+	Google.run();
 
-	TrieNode* root = new TrieNode();
-	root->insert("abcd", 2, 1);
-	root->insert("abcd", 8, 0);
-	root->insert("abc", 10, 0);
-
-	TrieNode* find = root->search("abcd", 0);
-	if (find) {
-		for (int i : find->occurs)
-			cout << i << ' ';
-		cout << '\n';
-	}
-	else cout << "not found\n";
-
-	system("pause");
+	//system("pause");
 }
