530a600078ec5fb5736972218af0ee6ce967b6c9
khoa <dknguyen176@gmail.com>
2021-07-21 11:30:22 +0700

    Replace gotoxy and remove TextColor

--- MiniSearchEngine/controllers/interface.cpp
+++ MiniSearchEngine/controllers/interface.cpp
@@ -26,18 +26,18 @@ void SearchEngine::run() {
 
     }
     
-    gotoxy(50, 50);
+    gotoXY(50, 50);
 }
 void SearchEngine::drawFrame(int a, int b) {
 
-    gotoxy(a, b);
+    gotoXY(a, b);
     cout << (char)196 << (char)196 << (char)196 << (char)196 << (char)196 << (char)196 << (char)196 << (char)196 << (char)196 << (char)196 << (char)196 << (char)196 << (char)196 << (char)196;
     cout << (char)196 << (char)196 << (char)196 << (char)196 << (char)196 << (char)196 << (char)196 << (char)196 << (char)196 << (char)196 << (char)196 << (char)196 << (char)196 << (char)196;
     cout << (char)196 << (char)196 << (char)196 << (char)196 << (char)196 << (char)196 << (char)196 << (char)196 << (char)196 << (char)196 << (char)196 << (char)196 << (char)196 << (char)196;
     cout << (char)196 << (char)196 << (char)196 << (char)196 << (char)196 << (char)196 << (char)196 << (char)196 << (char)196 << (char)196 << (char)196 << (char)196 << (char)196 << (char)196;
     cout << (char)196 << (char)196 << (char)196 << (char)196 << (char)196 << (char)196 << (char)196 << (char)196 << (char)196 << (char)196 << (char)196 << (char)196 << (char)196 << (char)196;
     cout << (char)196 << (char)196 << (char)196 << (char)196 << (char)196;
-    gotoxy(a, b + 8);
+    gotoXY(a, b + 8);
     cout << (char)196 << (char)196 << (char)196 << (char)196 << (char)196 << (char)196 << (char)196 << (char)196 << (char)196 << (char)196 << (char)196 << (char)196 << (char)196 << (char)196;
     cout << (char)196 << (char)196 << (char)196 << (char)196 << (char)196 << (char)196 << (char)196 << (char)196 << (char)196 << (char)196 << (char)196 << (char)196 << (char)196 << (char)196;
     cout << (char)196 << (char)196 << (char)196 << (char)196 << (char)196 << (char)196 << (char)196 << (char)196 << (char)196 << (char)196 << (char)196 << (char)196 << (char)196 << (char)196;
@@ -46,32 +46,28 @@ void SearchEngine::drawFrame(int a, int b) {
     cout << (char)196 << (char)196 << (char)196 << (char)196 << (char)196;
 
     for (int i = 1; i < 8; i++) {
-        gotoxy(a - 1, b + i);
+        gotoXY(a - 1, b + i);
         cout << (char)179;
     }
     for (int i = 1; i < 8; i++) {
-        gotoxy(a + 75, b + i);
+        gotoXY(a + 75, b + i);
         cout << (char)179;
     }
 
 }
 // search and suggestion frames
 void SearchEngine::draw(int x, int y) {
-    gotoxy(x, y);
+    gotoXY(x, y);
     cout << "SEARCH      :";
 
-    gotoxy(x, y + 2);
+    gotoXY(x, y + 2);
     cout << "SUGGESTION  :";
-}
-void SearchEngine::TextColor(int color)
-{
-    SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), color);
 }
 void SearchEngine::suggest(int coor, string data) {
     int a = 50;
     int b = 16;
 
-    gotoxy(a, b + coor * 1);
+    gotoXY(a, b + coor * 1);
     cout << data;
 }
 vector<string> SearchEngine::getSuggestion(vector<string>history, string text) {
@@ -86,108 +82,108 @@ void SearchEngine::drawGoogle(int x, int y) {
 
     //DRAW GOOGLE
 
-    gotoxy(5 + x, 1 + y);
+    gotoXY(5 + x, 1 + y);
     cout << (char)219 << (char)219;
     cout << (char)219 << (char)219 << (char)219;
 
-    gotoxy(4 + x, 2 + y);
+    gotoXY(4 + x, 2 + y);
     cout << (char)219 << (char)219;
-    gotoxy(3 + x, y + 3);
+    gotoXY(3 + x, y + 3);
     cout << (char)219 << (char)219 << char(219);
-    gotoxy(4 + x, 2 + y + 2);
+    gotoXY(4 + x, 2 + y + 2);
     cout << (char)219 << (char)219;
-    gotoxy(5 + x, 1 + y + 4);
+    gotoXY(5 + x, 1 + y + 4);
     cout << (char)219 << (char)219 << (char)219 << (char)219;
-    gotoxy(x + 9, y + 4);
+    gotoXY(x + 9, y + 4);
     cout << (char)219 << (char)219;
-    gotoxy(x + 8, y + 3);
+    gotoXY(x + 8, y + 3);
     cout << (char)219 << (char)219;
     //chu G
-    gotoxy(x + 16, y + 1);
+    gotoXY(x + 16, y + 1);
     cout << (char)219 << (char)219 << (char)219 << (char)219 << (char)219;
-    gotoxy(x + 14, y + 2);
+    gotoXY(x + 14, y + 2);
     cout << (char)219 << (char)219;
-    gotoxy(x + 14, y + 3);
+    gotoXY(x + 14, y + 3);
     cout << (char)219 << (char)219;
-    gotoxy(x + 14, y + 4);
+    gotoXY(x + 14, y + 4);
     cout << (char)219 << (char)219;
-    gotoxy(x + 16, y + 5);
+    gotoXY(x + 16, y + 5);
     cout << (char)219 << (char)219 << (char)219 << (char)219 << (char)219;
-    gotoxy(x + 21, y + 2);
+    gotoXY(x + 21, y + 2);
     cout << (char)219 << (char)219;
-    gotoxy(x + 21, y + 3);
+    gotoXY(x + 21, y + 3);
     cout << (char)219 << (char)219;
-    gotoxy(x + 21, y + 4);
+    gotoXY(x + 21, y + 4);
     cout << (char)219 << (char)219;
     //chu O
-    gotoxy(x + 32 - 3, y + 1);
+    gotoXY(x + 32 - 3, y + 1);
     cout << (char)219 << (char)219 << (char)219 << (char)219 << (char)219;
-    gotoxy(x + 30 - 3, y + 2);
+    gotoXY(x + 30 - 3, y + 2);
     cout << (char)219 << (char)219;
-    gotoxy(x + 30 - 3, y + 3);
+    gotoXY(x + 30 - 3, y + 3);
     cout << (char)219 << (char)219;
-    gotoxy(x + 30 - 3, y + 4);
+    gotoXY(x + 30 - 3, y + 4);
     cout << (char)219 << (char)219;
-    gotoxy(x + 32 - 3, y + 5);
+    gotoXY(x + 32 - 3, y + 5);
     cout << (char)219 << (char)219 << (char)219 << (char)219 << (char)219;
-    gotoxy(x + 37 - 3, y + 2);
+    gotoXY(x + 37 - 3, y + 2);
     cout << (char)219 << (char)219;
-    gotoxy(x + 37 - 3, y + 3);
+    gotoXY(x + 37 - 3, y + 3);
     cout << (char)219 << (char)219;
-    gotoxy(x + 37 - 3, y + 4);
+    gotoXY(x + 37 - 3, y + 4);
     cout << (char)219 << (char)219;
     //chu O
-    gotoxy(44 + x - 2, 1 + y);
+    gotoXY(44 + x - 2, 1 + y);
     cout << (char)219 << (char)219;
     cout << (char)219 << (char)219 << (char)219;
-    gotoxy(43 + x - 2, 2 + y);
+    gotoXY(43 + x - 2, 2 + y);
     cout << (char)219 << (char)219;
-    gotoxy(42 + x - 2, y + 3);
+    gotoXY(42 + x - 2, y + 3);
     cout << (char)219 << (char)219 << (char)219;
-    gotoxy(43 + x - 2, 2 + y + 2);
+    gotoXY(43 + x - 2, 2 + y + 2);
     cout << (char)219 << (char)219;
-    gotoxy(44 + x - 2, 1 + y + 4);
+    gotoXY(44 + x - 2, 1 + y + 4);
     cout << (char)219 << (char)219 << (char)219 << (char)219;
-    gotoxy(x + 48 - 2, y + 4);
+    gotoXY(x + 48 - 2, y + 4);
     cout << (char)219 << (char)219;
-    gotoxy(x + 47 - 2, y + 3);
+    gotoXY(x + 47 - 2, y + 3);
     cout << (char)219 << (char)219;
     //chu G
-    gotoxy(x + 52, y + 1);
+    gotoXY(x + 52, y + 1);
     cout << (char)219 << (char)219;
-    gotoxy(x + 52, y + 2);
+    gotoXY(x + 52, y + 2);
     cout << (char)219 << (char)219;
-    gotoxy(x + 52, y + 3);
+    gotoXY(x + 52, y + 3);
     cout << (char)219 << (char)219;
-    gotoxy(x + 52, y + 4);
+    gotoXY(x + 52, y + 4);
     cout << (char)219 << (char)219;
-    gotoxy(x + 52, y + 5);
+    gotoXY(x + 52, y + 5);
     cout << (char)219 << (char)219 << (char)219 << (char)219 << (char)219 << (char)219 << (char)219;
     //chu L
-    gotoxy(x + 64, y + 1);
+    gotoXY(x + 64, y + 1);
     cout << (char)219 << (char)219 << (char)219 << (char)219 << (char)219 << (char)219 << (char)219;
-    gotoxy(x + 64, y + 2);
+    gotoXY(x + 64, y + 2);
     cout << (char)219 << (char)219;
-    gotoxy(x + 64, y + 3);
+    gotoXY(x + 64, y + 3);
     cout << (char)219 << (char)219 << (char)219 << (char)219 << (char)219 << (char)219 << (char)219;
-    gotoxy(x + 64, y + 4);
+    gotoXY(x + 64, y + 4);
     cout << (char)219 << (char)219;
-    gotoxy(x + 64, y + 5);
+    gotoXY(x + 64, y + 5);
     cout << (char)219 << (char)219 << (char)219 << (char)219 << (char)219 << (char)219 << (char)219;
     //chu E
 
-    gotoxy(x + 10, y + 10);
+    gotoXY(x + 10, y + 10);
 
 }
 void SearchEngine::readInput(vector<string>& history, string& text, int x, int y, bool& stop) {
     char character;
 
     for (int i = 0; i < 30; i++) {
-        gotoxy(x + i, y);
+        gotoXY(x + i, y);
         cout << ' ';
     }
     while (!text.empty()) text.pop_back();
-    gotoxy(x, y);
+    gotoXY(x, y);
     vector<string> t;
     while (character = _getch(), (int)character != 13) {
         if ((int)character == 27) {
@@ -197,8 +193,8 @@ void SearchEngine::readInput(vector<string>& history, string& text, int x, int y
         if (character == '\b') {
             if (text.size() == 0) continue;
             text.pop_back();
-            gotoxy(x + (int)text.size(), y); cout << ' ';
-            gotoxy(x + (int)text.size(), y);
+            gotoXY(x + (int)text.size(), y); cout << ' ';
+            gotoXY(x + (int)text.size(), y);
 
         }
         // keyUp keyDown on suggestion
@@ -235,7 +231,7 @@ void SearchEngine::readInput(vector<string>& history, string& text, int x, int y
         //clear all suggestion
         for (int i = 0; i < 10; i++) {
             for (int j = 0; j < 30; j++) {
-                gotoxy(50 + j, (i + 1) * 1 + 16);
+                gotoXY(50 + j, (i + 1) * 1 + 16);
                 cout << ' ';
             }
         }
@@ -245,9 +241,9 @@ void SearchEngine::readInput(vector<string>& history, string& text, int x, int y
                 suggest(i + 1, t[i]);
             }
         }
-        //gotoxy(x, y);
+        //gotoXY(x, y);
 
-        gotoxy(x + (int)text.size(), y);
+        gotoXY(x + (int)text.size(), y);
     }
 
 
@@ -275,10 +271,10 @@ void SearchEngine::showResult(vector<result> results, string text, set<string> w
 
 }
 vector<string> SearchEngine::getReview(string filename, set<string> words, int maxLength) {
-
+    return vector<string>();
 }
 vector<string> SearchEngine::getContent(string filename, int maxLength) {
-
+    return vector<string>();
 }
 
 
