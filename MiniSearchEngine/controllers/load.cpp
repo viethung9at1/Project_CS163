c55cd22ffc5c45058660e62ef176ab470c2988cb
viethung9at1 <viethung.9at1@gmail.com>
2021-07-22 05:05:06 +0700

    Update

--- MiniSearchEngine/controllers/load.cpp
+++ MiniSearchEngine/controllers/load.cpp
@@ -1,8 +1,10 @@
 #include "../models/SearchEngine.h"
-
+#include<fstream>
+using namespace std;
 void SearchEngine::loadData() { // Load files, stopwords
-
+	ifstream fin;
+	
 }
-void SearchEngine::loadFile(TrieNode* root, string filename) {
-
+void SearchEngine::loadFile(TrieNode*& root, string filename) {
+	
 }
