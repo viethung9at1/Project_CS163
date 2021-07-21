73a9f9c44246d2f6e425894edf0eb995474394dc
viethung9at1 <viethung.9at1@gmail.com>
2021-07-22 05:32:00 +0700

    Import data

--- MiniSearchEngine/controllers/load.cpp
+++ MiniSearchEngine/controllers/load.cpp
@@ -1,10 +1,22 @@
-#include "../models/SearchEngine.h"
-#include<fstream>
-using namespace std;
-void SearchEngine::loadData() { // Load files, stopwords
-	ifstream fin;
-	
-}
-void SearchEngine::loadFile(TrieNode*& root, string filename) {
-	
+//#include "../models/SearchEngine.h"
+//#include<fstream>
+//#include<filesystem>
+//using namespace std;
+//void SearchEngine::loadData() { // Load files, stopwords
+//	ifstream fin;
+//	using (fs=filesystem)
+//}
+//void SearchEngine::loadFile(TrieNode*& root, string filename) {
+//	
+//}
+
+#include <string>
+#include <iostream>
+#include <filesystem>
+namespace fs = std::exprt;
+
+int main() {
+    std::string path = "/path/to/directory";
+    for (const auto& entry : fs::directory_iterator(path))
+        std::cout << entry.path() << std::endl;
 }
