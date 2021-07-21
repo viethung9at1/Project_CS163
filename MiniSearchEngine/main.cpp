a91964bd8247c869ce844bf135c9e7a50699d245
khoa <dknguyen176@gmail.com>
2021-07-21 11:00:39 +0700

    Update main.cpp
    Nothing just test

--- MiniSearchEngine/main.cpp
+++ MiniSearchEngine/main.cpp
@@ -5,16 +5,17 @@ using namespace std;
 
 int main()
 {
-	// SearchEngine Google;
+	SearchEngine Google;
 	// Google.loadData();
 	// Google.run();
+	vector<result> results = Google.searchQuery("steve jobs");
 
 	TrieNode* root = new TrieNode();
 	root->insert("abcd", 2, 1);
 	root->insert("abcd", 8, 0);
 	root->insert("abc", 10, 0);
 
-	TrieNode* find = root->search("abc", 0);
+	TrieNode* find = root->search("abcd", 0);
 	if (find) {
 		for (int i : find->occurs)
 			cout << i << ' ';
