ab12f78d77cd7e78216427b41cb4b0758ef371ed
viethung9at1 <viethung.9at1@gmail.com>
2021-07-22 15:09:18 +0700

    Test filesystem function

--- MiniSearchEngine/controllers/load.cpp
+++ MiniSearchEngine/controllers/load.cpp
@@ -5,7 +5,9 @@ using namespace std;
 namespace fs = std::filesystem;
 void SearchEngine::loadData() { // Load files, stopwords
 	ifstream fin;
-	string path=""
+	string path = "//DataSearch";
+	for (const auto& entry : fs::directory_iterator(path))
+		std::cout << entry.path() << std::endl;
 }
 void SearchEngine::loadFile(TrieNode*& root, string filename) {
 	
