c363a8302cc24f3db3fbd058002fad2edb0d5f1e
viethung9at1 <viethung.9at1@gmail.com>
2021-07-22 21:57:16 +0700

    Update interface and load

--- MiniSearchEngine/controllers/load.cpp
+++ MiniSearchEngine/controllers/load.cpp
@@ -1,12 +1,17 @@
 #include "../models/SearchEngine.h"
 #include<fstream>
-#include<filesystem>
+#include<dirent.h>
 using std::string;
-namespace fs = std::filesystem;
 void SearchEngine::loadData() { // Load files, stopwords
-	string path = "DataSearch";
+	DIR* dir;
+	struct dirent* ent;
+	if ((dir = opendir("DataSearch")) != NULL) {
+		while ((ent = readdir(dir)) != NULL) 
+			filenames.push_back(ent->d_name);
+		closedir(dir);
+	}
 
 }
 void SearchEngine::loadFile(TrieNode*& root, string filename) {
-	
+
 }
