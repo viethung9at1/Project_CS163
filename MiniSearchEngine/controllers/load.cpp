1656e9b88c7e7d74b5499eefa05077fd248a1c30
viethung9at1 <viethung.9at1@gmail.com>
2021-07-22 20:59:09 +0700

    Update load

--- MiniSearchEngine/controllers/load.cpp
+++ MiniSearchEngine/controllers/load.cpp
@@ -1,13 +1,7 @@
 #include "../models/SearchEngine.h"
 #include<fstream>
-#include<filesystem>
 using namespace std;
-namespace fs = std::filesystem;
 void SearchEngine::loadData() { // Load files, stopwords
-	ifstream fin;
-	string path = "//DataSearch";
-	for (const auto& entry : fs::directory_iterator(path))
-		std::cout << entry.path() << std::endl;
 }
 void SearchEngine::loadFile(TrieNode*& root, string filename) {
 	
