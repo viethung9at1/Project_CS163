592e6b657ab926fcef2d6d46668898f1bab0ebe7
viethung9at1 <viethung.9at1@gmail.com>
2021-07-23 00:03:13 +0700

    Load syno, stopword

--- MiniSearchEngine/controllers/load.cpp
+++ MiniSearchEngine/controllers/load.cpp
@@ -1,17 +1,53 @@
 #include "../models/SearchEngine.h"
 #include<fstream>
 #include<dirent.h>
-using std::string;
+using namespace std;
 void SearchEngine::loadData() { // Load files, stopwords
 	DIR* dir;
 	struct dirent* ent;
 	if ((dir = opendir("DataSearch")) != NULL) {
-		while ((ent = readdir(dir)) != NULL) 
+		while ((ent = readdir(dir)) != NULL)
+		{
 			filenames.push_back(ent->d_name);
+
+			//load file
+			data[ent->d_name] = NULL;
+			loadFile(data[ent->d_name], ent->d_name);
+		}
 		closedir(dir);
 	}
-
+	ifstream fin;
+	fin.open("synonyms.txt");
+	if (fin) {
+		while (!fin.eof()) {
+			string s, addToSyn = "";
+			vector<string> syn;
+			getline(fin,s);
+			for (int i = 0; i < s.length(); i++) {
+				if(s[i]!=' ') addToSyn += s[i];
+				else {
+					syn.push_back(addToSyn);
+					synoMap[syn[0]]++;
+					addToSyn = "";
+				}
+			}
+			synoList.push_back(syn);
+		}
+	}
+	fin.close();
+	fin.open("stopwords.txt");
+	stopWords = new TrieNode();
+	if (fin) {
+		int i = 0;
+		while (!fin.eof()) {
+			string s;
+			fin >> s;
+			stopWords->insert(s, i++, i == 0 ? true : false);
+		}
+	}
+	fin.close();
 }
 void SearchEngine::loadFile(TrieNode*& root, string filename) {
+	root = new TrieNode();
 
 }
--- MiniSearchEngine/main.cpp
+++ MiniSearchEngine/main.cpp
@@ -1,13 +1,16 @@
 #include<iostream>
 #include "models/SearchEngine.h"
+#include<fstream>
 
 using namespace std;
+void a(int i, bool f)
+{
 
+}
 int main()
 {
 	SearchEngine Google;
 	Google.loadData();
 	Google.run();
-
 	//system("pause");
 }
