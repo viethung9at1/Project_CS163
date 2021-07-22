75130a88f5f43c562d4d259e4eb5d2f8bd529f97
viethung9at1 <viethung.9at1@gmail.com>
2021-07-22 15:07:18 +0700

    Change data folder

--- MiniSearchEngine/controllers/load.cpp
+++ MiniSearchEngine/controllers/load.cpp
@@ -2,8 +2,10 @@
 #include<fstream>
 #include<filesystem>
 using namespace std;
+namespace fs = std::filesystem;
 void SearchEngine::loadData() { // Load files, stopwords
 	ifstream fin;
+	string path=""
 }
 void SearchEngine::loadFile(TrieNode*& root, string filename) {
 	
